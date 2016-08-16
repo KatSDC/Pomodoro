dac8b41ddf83e95dc3bc8e9e7882c1b1f92c1d6e
Kathryn Schroeder <schroeder.kat@gmail.com>
2016-08-16 12:40:55 -0400

    Adding styling and second integration with Toggl

--- js/index.js
+++ js/index.js
@@ -30,15 +30,15 @@ function displayWorkTimer() {
 	minutes = Math.floor(workTime / 60);
 	seconds = workTime % 60;
 	document.getElementById('timer').innerHTML = pad(minutes) + ":" + pad(seconds);
+	loadFeed();
 
 	$('#start').on('click', start);
 	$('#pause').on('click', pause);
-	$('#jira').on('submit', displayBreak);
+	$('#jira').on('submit', submitWork);
 }
 
 //This function displays the break timer no the page and sets up event listeners. It also hides the JIRA form.
-function displayBreak(event) {
-	event.preventDefault();
+function displayBreak() {
 	breakTime = 300;
 	minutes = Math.floor(breakTime / 60);
 	seconds = breakTime % 60;
@@ -90,6 +90,25 @@ function pause() {
 	document.getElementById('start').disabled = false;
 }
 
+//This function will submit the work log to Toggl
+function submitWork() {
+	event.preventDefault();
+	$.ajax({
+  		type: "POST",
+  		url: "https://cors-anywhere.herokuapp.com/https://www.toggl.com/api/v8/time_entries",
+  		beforeSend: function(xhr) {
+  			xhr.setRequestHeader("Authorization", "Basic " + btoa("8e3667b730cde20d46dc7effcede90e5:api_token"))
+  		},
+  		dataType: "json",
+  		contentType: "application/json",
+ 		processData: false,
+ 		data: '{"time_entry":{"description":"Logging time to ticket","duration":1500,"start":"2016-08-15T20:24:55+00:00","pid":21218612,"created_with":"pomodoro"}}',
+  		success: function() {
+  			displayBreak();
+  		},
+	});
+}
+
 //Sets up the interval and starts the break timer
 function startBreak() {
 	countdownId = setInterval("countdownBreak()", 1000);
@@ -99,7 +118,6 @@ function startBreak() {
 function countdownBreak() {
 	minutes = Math.floor(breakTime / 60);
 	seconds = breakTime % 60;
-	loadFeed();
 	$('#reddit').css('visibility', 'visible');
 	if (breakTime > 0) {
 		breakTime = breakTime - 1;
@@ -126,4 +144,38 @@ function loadFeed() {
 }
 
 $(document).ready(displayWorkTimer);
+
+
+// Working API GET Request!:
+// curl -v -u 8e3667b730cde20d46dc7effcede90e5:api_token \
+//    -H "Content-Type: application/json" \
+//    -H "Origin: localhost" \
+//     -X GET https://cors-anywhere.herokuapp.com/https://www.toggl.com/api/v8/projects/21218612
+
+// AJAX Requestion based on following curl request:
+//curl -v -u 1971800d4d82861d8f2c1651fea4d212:api_token \
+//    -H "Content-Type: application/json" \
+//    -d '{"time_entry":{"description":"Meeting with possible clients","tags":["billed"],"duration":1200,"start":"2013-03-05T07:58:58.000Z","pid":123,"created_with":"curl"}}' \
+//    -X POST https://www.toggl.com/api/v8/time_entries
+
+
+//This request is returning a 403 error in the console.
+//$.ajax({
+//  type: "POST",
+//  dataType: "json",
+//  url: "https://cors-anywhere.herokuapp.com/https://www.toggl.com/api/v8/time_entries",
+//  contentType: "application/json",
+//  username: "8e3667b730cde20d46dc7effcede90e5",
+//  password: "api_token",
+//  data: {"time_entry":{"description":"Logging time to ticket","duration":1500,"start":"2016-08-15T20:24:55+00:00","pid":21218612,"created_with":"pomodoro"}},
+//  success: function(response){console.log(response)},
+//});
+
+// This POST request works in curl
+//curl -v -u 8e3667b730cde20d46dc7effcede90e5:api_token \
+//    -H "Content-Type: application/json" \
+//    -H "Origin: localhost" \
+//    -d '{"time_entry":{"description":"Logging time to ticket","duration":1500,"start":"2016-08-15T20:24:55+00:00","pid":21218612,"created_with":"pomodoro"}}' \
+//    -X POST https://cors-anywhere.herokuapp.com/https://www.toggl.com/api/v8/time_entries
+
 
